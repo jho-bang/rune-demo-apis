9f072d57c97487c707249e57aa2243d1b05bc8b3
Jaeho Bang <jhb@marpple.com>
2024-05-17 10:36:34 +0900

    update

--- src/db/schema/demo/index.ts
+++ src/db/schema/demo/index.ts
@@ -2,6 +2,5 @@ import type { CommonSchema } from "../common";
 
 export interface DemoSchema extends CommonSchema {
   origin_src: string;
-  new_src: string;
   user_id: number;
 }
--- src/db/schema/user/index.ts
+++ src/db/schema/user/index.ts
@@ -1,7 +1,6 @@
 import type { CommonSchema } from "../common";
 
 export interface UserSchema extends CommonSchema {
-  user_id: number;
   sns_id: number;
   sns: string;
   username: string;
